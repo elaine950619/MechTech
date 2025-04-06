4b4e81c465f0eae68626930c04c7f1e34fee8a3a
elaine950619 <yishun.hsieh@gmail.com>
2025-04-05 23:56:37 -0400

    current

--- Project_2/schema.sql
+++ Project_2/schema.sql
@@ -9,14 +9,3 @@ CREATE TABLE IF NOT EXISTS news (
     sentiment_neutral REAL,
     sentiment_compound REAL
 ); 
-
-DROP TABLE IF EXISTS aggregated_data;
-CREATE TABLE aggregated_data (
-    id SERIAL PRIMARY KEY,
-    published_date DATE,
-    avg_sentiment_positive REAL,
-    avg_sentiment_negative REAL,
-    avg_sentiment_neutral REAL,
-    avg_sentiment_compound REAL,
-    closing_price REAL
-);
